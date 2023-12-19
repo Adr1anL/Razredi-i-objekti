9e3cc8c424d4f38179b0fa3c88d207390ef99ace
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-12-19 10:58:55 +0100

    Add files via upload

---
+++ BeogradskiNeboderi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class BeogradskiNeboderi
+    {
+        double visina;
+        int brojKatova;
+
+        public double Visina { get => visina; set => visina = value; }
+        public int BrojKatova { get => brojKatova; set => brojKatova = value; }
+
+        public BeogradskiNeboderi(double visina, int brojKatova)
+        {
+            this.Visina = visina;
+            this.BrojKatova = brojKatova;
+        }
+
+        public BeogradskiNeboderi()
+        {
+            this.Visina = visina;
+            this.BrojKatova = brojKatova;
+        }
+
+        public override string ToString()
+        {
+            visina = visina + 10;
+
+            brojKatova = brojKatova + 1;
+
+            double prosjek = visina / brojKatova;
+
+            string beograd = "Visina: " + visina + " m\nBroj katova: " + brojKatova + "\nProsječna visina kata: " + prosjek + " m";
+
+            return beograd;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeogradskiNeboderi beogradskiNeboderi = new BeogradskiNeboderi(200, 100);
+
+            Console.WriteLine(beogradskiNeboderi.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
